395d4e953839cbad45b9c265e26014d687cf88e7
wdxxs2z <243219650@qq.com>
2018-04-12 22:05:35 +0800

    chart support: 1. add kubeconfig set. 2. add rbac. 3. fix init_helm.sh catalog config.

--- scripts/init_helm.sh
+++ scripts/init_helm.sh
@@ -3,6 +3,9 @@ if [ "$HELM_REPO_URL" != "" ] && [ "$HELM_REPO_NAME" != "" ]; then
     helm init --client-only && \
     helm repo add $HELM_REPO_NAME $HELM_REPO_URL && \
     helm repo update
+else
+    helm init --client-only && \
+    helm repo update
 fi
 # Make sure the catalog.yaml
 if [ -a /app/config/catalog.yaml ]; then
