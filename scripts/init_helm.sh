22ba72ab1d9c27e2fee7fda3d6b90d23b959c930
wdxxs2z <243219650@qq.com>
2018-04-13 09:30:45 +0800

    update ingress secret.

--- scripts/init_helm.sh
+++ scripts/init_helm.sh
@@ -1,4 +1,5 @@
-# Initialize helm
+#!/bin/sh
+
 if [ "$HELM_REPO_URL" != "" ] && [ "$HELM_REPO_NAME" != "" ]; then
     helm init --client-only && \
     helm repo add $HELM_REPO_NAME $HELM_REPO_URL && \
@@ -7,9 +8,9 @@ else
     helm init --client-only && \
     helm repo update
 fi
-# Make sure the catalog.yaml
+
 if [ -a /app/config/catalog.yaml ]; then
     cp /app/config/catalog.yaml /app/
 fi
-# Start helmi
+
 helmi
