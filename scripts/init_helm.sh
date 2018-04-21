7cf552b6bb4169d83370b331a3d634b3795f2a2e
wdxxs2z <243219650@qq.com>
2018-04-21 12:29:44 +0800

    fix the configmap lookup values.

--- scripts/init_helm.sh
+++ scripts/init_helm.sh
@@ -10,6 +10,8 @@ else
 fi
 
 if [ -f /app/config/catalog.yaml ]; then
+    sed -i 's/\\{\\{/{{/g' /app/config/catalog.yaml
+    sed -i 's/\\}\\}/}}/g' /app/config/catalog.yaml
     cp /app/config/catalog.yaml /app/
 fi
 
